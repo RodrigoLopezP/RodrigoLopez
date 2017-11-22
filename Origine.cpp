0363118fa2df1c85f9af5e40ac964dd656253ecf
RodrigoLopezP <rodrigolopezp98@gmail.com>
2017-11-22 04:39:09 +0100

    CodiceCoutINIZIOmod

--- Origine.cpp
+++ Origine.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void main() {
 	int contatore = 0;
-	cout<<"INIZIO"<<endl;
+	cout<<"Da questo punto inizia il ciclo da 1 a 10"<<endl;
 	for(int i=0; i<10;i++){
 		contatore++;
 		cout << contatore << endl;
