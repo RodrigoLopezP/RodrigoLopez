3fb799aba0763db2882b3b9cde0b719f30cb8a71
RodrigoLopezP <rodrigolopezp98@gmail.com>
2017-11-22 04:51:16 +0100

    CommitCommento1

--- Origine.cpp
+++ Origine.cpp
@@ -4,12 +4,12 @@ using namespace std;
 void main() {
 	int contatore = 0;
 	cout<<"Da questo punto inizia il ciclo da 1 a 10"<<endl;
-/*Inizio ciclo */
+/*Inizio  */
 	for(int i=0; i<10;i++){
 		contatore++;
 		cout << contatore << endl;
 	}
-/*Fine ciclo */
+/*Fine */
 	cout<<"Il ciclo è finito"<<endl;
 	system("pause");
 }
